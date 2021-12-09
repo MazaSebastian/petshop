c620500c47239bba9c359d6695f0d0194778704c
sebamaza <mazasebastian@hotmail.com>
2021-12-09 12:54:12 -0300

    itemlist

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Header from './components/Header/Header';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Footer from './components/Footer/Footer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <ItemDetailContainer/>
+      <ItemListContainer greeting="Bienvenido a PetShop, lo mejor para tu mascota!"/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
